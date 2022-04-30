093a178cf89efe92172cb76de0b164ac72c2bd37
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-04-30 10:34:20 +0200

    Projects Section

---
+++ components/projects/Projects.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+import styles from "./Projects.module.css";
+
+const Projects: NextPage = () => {
+  return <section className={styles.section}>Projects</section>;
+};
+
+export default Projects;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import About from "../components/about/About";
 import Home from "../components/home/Home";
+import Projects from "../components/projects/Projects";
 import Skills from "../components/skills/Skills";
 import styles from "../styles/Home.module.css";
 
@@ -17,6 +18,7 @@ const Index: NextPage = () => {
       <Home />
       <About />
       <Skills />
+      <Projects />
     </div>
   );
 };
